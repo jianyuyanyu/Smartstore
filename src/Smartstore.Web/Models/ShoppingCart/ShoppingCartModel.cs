d4c163d54a558c4301abc9eb1d50ea0057c2e27f
mgesing <gesing@smartstore.com>
2026-04-30 13:05:49 +0200

    Fixed broken conflict merging

--- src/Smartstore.Web/Models/ShoppingCart/ShoppingCartModel.cs
+++ src/Smartstore.Web/Models/ShoppingCart/ShoppingCartModel.cs
@@ -55,6 +55,7 @@ public partial class ShoppingCartModel : CartModelBase
         /// It can only be removed from the cart when the parent product is removed, if <c>true</c>.
         /// </summary>
         public bool IsRequired { get; set; }
+        public bool DisableQuantityControl { get; set; }
     }
 
     public partial class CheckoutAttributeModel : ChoiceModel
@@ -62,13 +63,6 @@ public partial class ShoppingCartModel : CartModelBase
         public override string BuildControlId()
             => CheckoutAttributeQueryItem.CreateKey(Id);
 
-            /// <summary>
-            /// Gets or sets a value indicating whether the item is an auto-added, required product.
-            /// It can only be removed from the cart when the parent product is removed, if <c>true</c>.
-            /// </summary>
-            public bool IsRequired { get; set; }
-            public bool DisableQuantityControl { get; set; }
-        }
         public override string GetFileUploadUrl(IUrlHelper url)
             => url.Action("UploadFileCheckoutAttribute", "ShoppingCart", new { controlId = BuildControlId() });
     }
